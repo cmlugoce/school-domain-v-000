bdb15e8bf60a99620ba3a94ab4f4458ca143347d
Cristina Lugo <cmlugoce@syr.edu>
2018-06-14 13:41:42 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,14 +9,9 @@ def roster
 end  
 
 def add_student(student, grade)
-  @student = student 
-  @grade = grade 
-  
-  if @roster[grade] == nil 
-    @roster[grade] =[student]
-    
-  else @roster[grade] << student 
-end 
+  roster[grade] ||= []
+  roster[grade] << student
+  end
  end 
  
  def grade(grade)
